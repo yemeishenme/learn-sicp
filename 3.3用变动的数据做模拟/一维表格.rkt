c4498bf30b83e271a4fe07989454c5f37c35de53
yemeishenme@163.com <yemeishenme@163.com>
2016-12-27 18:22:34 +0800

    创建局部表格，没有写完

--- 3.3用变动的数据做模拟/一维表格.rkt
+++ 3.3用变动的数据做模拟/一维表格.rkt
@@ -26,20 +26,22 @@
   (let ((record (assoc key (cdr table)))) ; 查找对应的值
     (if record                            ; 如查找到
         (set-cdr! record value)           ; 设置其值为value
-        (set-cdr! table                   ; 将该键值对儿接在表的开头位置
+        (set-cdr! table                   ; 将该键值对儿接在表的开头位置(*table*的后面)
                   (cons (cons key value) (cdr table)))))
   'ok)
 
-(define (insert-tail! key value table)
+;#|
+(define (insert-tail-t! key value table)
   (let ((record (assoc key (cdr table)))) ; 查找对应的值
     (if record                            ; 如查找到
         (set-cdr! record value)           ; 设置其值为value
-        (if (<= (length table) 1)
+        (if (<= (length table) 1)         ; 如果表的长度小于等于1，说明表中没有元素
             (set-cdr! table
-                      (cons (cons key value)) (cdr table))
+                      (cons (cons key value)))
             (append table                   ; 将该键值对儿接在表的开头位置
                       (cons key value)))))
   'ok)
+;|#
 #|
 构造新表:创建一个包含符号*table*的表
 ;|#
